8870649a62bac919a8521fbf0f2d8637dce2c196
zhangfeng <zhangfeng@doov.com.cn>
2017-03-03 15:15:36 +0800

    1.fix add devcie api return json format.

--- mobile/account_add.php
+++ mobile/account_add.php
@@ -11,7 +11,8 @@ $sql = "SELECT name FROM accounts WHERE name='" . $_GET["name"] . "'";
 $result = $link->query($sql);
 
 if ($result->num_rows > 0) {
-    echo "0";
+    $arr = array('result' => 0);
+    echo json_encode($arr);
 }else {
     $sql = "INSERT INTO accounts (name, password, phone, birthday, weight, height) VALUES ('" . $_GET["name"] . "', '" . $_GET["password"] . "', " . $_GET["phone"] . ", " . $_GET["birthday"] . ", " . $_GET["weight"] . ", " . $_GET["height"] . ")";
 
